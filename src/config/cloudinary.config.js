f49d609408b68d985229c417f980e936fb5c5b15
miniaggarwal <priyankaagarwal608@gmail.com>
2025-04-04 22:29:01 +0530

    Second commit

--- src/config/cloudinary.config.js
+++ src/config/cloudinary.config.js
@@ -5,5 +5,7 @@ export const cloudinary_config = cloudinary.config({
     cloud_name : config.CLOUDINARY_NAME,
     api_key : config.CLOUDINARY_API,
     api_secret : config.CLOUDINARY_SECRET,
+    //fhgjfhgjh
+    
 
 })
